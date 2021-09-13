b91d559c80ffd5a6281992e2d091fc37cf9f843b
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2021-09-13 23:11:13 +0500

    model

---
+++ Models/Disk.cs
@@ -0,0 +1,27 @@
+﻿namespace Mvc.Models
+{
+    public class Disk
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SizeGb { get; set; }
+        public int WriteSpeedMbps { get; set; }
+        public int ReadSpeedMbps { get; set; }
+        public decimal Price { get; set; }
+        public DiskType Type { get; set; }
+        public DiskForm Form { get; set; }
+    }
+
+    public enum DiskType
+    {
+        SSD,
+        HDD,
+    }
+
+    public enum DiskForm
+    {
+        Sata25,
+        Sata35,
+        M2,
+    }
+}
