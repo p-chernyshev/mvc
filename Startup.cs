ab4ab206e021075677e62e01f7a1062afdfe8c05
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2021-09-13 23:27:34 +0500

    add DB, initial migration

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Mvc.Data;
 
 namespace Mvc
 {
@@ -24,6 +26,9 @@ namespace Mvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcDiskContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcDiskContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
