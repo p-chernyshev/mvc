28b6906713cbe42c96bffa177f29ad7bd7109582
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2021-09-14 22:40:48 +0500

    disk controller, index view

---
+++ Controllers/DiskController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Mvc.Data;
+using Mvc.Models;
+
+namespace Mvc.Controllers
+{
+    public class DiskController : Controller
+    {
+        private readonly MvcDiskContext _context;
+
+        public DiskController(MvcDiskContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Disk.ToListAsync());
+        }
+    }
+}
