d6f27fe2de8ae3e9d0f0f5edeca26d0b511811e0
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2021-09-15 23:37:07 +0500

    sort

--- Controllers/DiskController.cs
+++ Controllers/DiskController.cs
@@ -21,9 +21,21 @@ namespace Mvc.Controllers
 
         public async Task<IActionResult> Index(string sortBy = "Id", string sortDirection = "asc")
         {
+            var sortedQuery = sortBy switch
+            {
+                "Name" => sortDirection == "asc" ? _context.Disk.OrderBy(disk => disk.Name) : _context.Disk.OrderByDescending(disk => disk.Name),
+                "SizeGb" => sortDirection == "asc" ? _context.Disk.OrderBy(disk => disk.SizeGb) : _context.Disk.OrderByDescending(disk => disk.SizeGb),
+                "WriteSpeedMbps" => sortDirection == "asc" ? _context.Disk.OrderBy(disk => disk.WriteSpeedMbps) : _context.Disk.OrderByDescending(disk => disk.WriteSpeedMbps),
+                "ReadSpeedMbps" => sortDirection == "asc" ? _context.Disk.OrderBy(disk => disk.ReadSpeedMbps) : _context.Disk.OrderByDescending(disk => disk.ReadSpeedMbps),
+                "Price" => sortDirection == "asc" ? _context.Disk.OrderBy(disk => disk.Price) : _context.Disk.OrderByDescending(disk => disk.Price),
+                "Type" => sortDirection == "asc" ? _context.Disk.OrderBy(disk => disk.Type) : _context.Disk.OrderByDescending(disk => disk.Type),
+                "Form" => sortDirection == "asc" ? _context.Disk.OrderBy(disk => disk.Form) : _context.Disk.OrderByDescending(disk => disk.Form),
+                _ => sortDirection == "asc" ? _context.Disk.OrderBy(disk => disk.Id) : _context.Disk.OrderByDescending(disk => disk.Id),
+            };
+
             return View(new DiskListViewModel
             {
-                Disks = await _context.Disk.ToListAsync(),
+                Disks = await sortedQuery.ToListAsync(),
                 SortBy = sortBy,
                 SortDirection = sortDirection,
             });
