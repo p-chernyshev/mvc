02f06cb8d9986539baab4d2d9ee61af9a483e2d1
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2021-09-22 22:17:08 +0500

    add city, migration

--- Migrations/20210922171538_City.Designer.cs
+++ Migrations/20210922171538_City.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mvc.Data;
 
 namespace Mvc.Migrations
 {
     [DbContext(typeof(MvcDiskContext))]
-    partial class MvcDiskContextModelSnapshot : ModelSnapshot
+    [Migration("20210922171538_City")]
+    partial class City
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
