c71fc614431a0d93c6b28ef12a375a55fbd6df84
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2021-09-26 16:54:02 +0500

    show popup until city selected

--- Controllers/DiskController.cs
+++ Controllers/DiskController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Mvc.Data;
@@ -17,6 +18,12 @@ namespace Mvc.Controllers
         public DiskController(MvcDiskContext context)
         {
             _context = context;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+            ViewData["SelectedCity"] = Request.Cookies["City"];
         }
 
         public async Task<IActionResult> Index(string sortBy = "Id", string sortDirection = "asc", string search = "")
