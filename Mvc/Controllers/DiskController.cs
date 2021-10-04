d9c705a2c8dc12c38aa96c4dcd6613a78e7d3f5d
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2021-10-04 23:18:19 +0500

    add disk list json get method

--- Mvc/Controllers/DiskController.cs
+++ Mvc/Controllers/DiskController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -34,29 +35,57 @@ namespace Mvc.Controllers
 
         public async Task<IActionResult> Index(string sortBy = "Id", string sortDirection = "asc", string search = "")
         {
+            return View(new DiskListViewModel
+            {
+                Disks = await DisksList(sortBy, sortDirection, search),
+                SortBy = sortBy,
+                SortDirection = sortDirection,
+                Search = search,
+            });
+        }
+
+        [EnableCors("Localhost")]
+        [HttpGet]
+        public async Task<IActionResult> IndexJson(string sortBy = "Id", string sortDirection = "asc", string search = "")
+        {
+            return Json(await DisksList(sortBy, sortDirection, search));
+        }
+
+        private Task<List<Disk>> DisksList(string sortBy, string sortDirection, string search)
+        {
             var filteredQuery = string.IsNullOrEmpty(search)
                 ? _context.Disk
                 : _context.Disk.Where(disk => disk.Name.Contains(search));
 
             var sortedQuery = sortBy switch
             {
-                "Name" => sortDirection == "asc" ? filteredQuery.OrderBy(disk => disk.Name) : filteredQuery.OrderByDescending(disk => disk.Name),
-                "SizeGb" => sortDirection == "asc" ? filteredQuery.OrderBy(disk => disk.SizeGb) : filteredQuery.OrderByDescending(disk => disk.SizeGb),
-                "WriteSpeedMbps" => sortDirection == "asc" ? filteredQuery.OrderBy(disk => disk.WriteSpeedMbps) : filteredQuery.OrderByDescending(disk => disk.WriteSpeedMbps),
-                "ReadSpeedMbps" => sortDirection == "asc" ? filteredQuery.OrderBy(disk => disk.ReadSpeedMbps) : filteredQuery.OrderByDescending(disk => disk.ReadSpeedMbps),
-                "Price" => sortDirection == "asc" ? filteredQuery.OrderBy(disk => disk.Price) : filteredQuery.OrderByDescending(disk => disk.Price),
-                "Type" => sortDirection == "asc" ? filteredQuery.OrderBy(disk => disk.Type) : filteredQuery.OrderByDescending(disk => disk.Type),
-                "Form" => sortDirection == "asc" ? filteredQuery.OrderBy(disk => disk.Form) : filteredQuery.OrderByDescending(disk => disk.Form),
-                _ => sortDirection == "asc" ? filteredQuery.OrderBy(disk => disk.Id) : filteredQuery.OrderByDescending(disk => disk.Id),
+                "Name" => sortDirection == "asc"
+                    ? filteredQuery.OrderBy(disk => disk.Name)
+                    : filteredQuery.OrderByDescending(disk => disk.Name),
+                "SizeGb" => sortDirection == "asc"
+                    ? filteredQuery.OrderBy(disk => disk.SizeGb)
+                    : filteredQuery.OrderByDescending(disk => disk.SizeGb),
+                "WriteSpeedMbps" => sortDirection == "asc"
+                    ? filteredQuery.OrderBy(disk => disk.WriteSpeedMbps)
+                    : filteredQuery.OrderByDescending(disk => disk.WriteSpeedMbps),
+                "ReadSpeedMbps" => sortDirection == "asc"
+                    ? filteredQuery.OrderBy(disk => disk.ReadSpeedMbps)
+                    : filteredQuery.OrderByDescending(disk => disk.ReadSpeedMbps),
+                "Price" => sortDirection == "asc"
+                    ? filteredQuery.OrderBy(disk => disk.Price)
+                    : filteredQuery.OrderByDescending(disk => disk.Price),
+                "Type" => sortDirection == "asc"
+                    ? filteredQuery.OrderBy(disk => disk.Type)
+                    : filteredQuery.OrderByDescending(disk => disk.Type),
+                "Form" => sortDirection == "asc"
+                    ? filteredQuery.OrderBy(disk => disk.Form)
+                    : filteredQuery.OrderByDescending(disk => disk.Form),
+                _ => sortDirection == "asc"
+                    ? filteredQuery.OrderBy(disk => disk.Id)
+                    : filteredQuery.OrderByDescending(disk => disk.Id),
             };
 
-            return View(new DiskListViewModel
-            {
-                Disks = await sortedQuery.ToListAsync(),
-                SortBy = sortBy,
-                SortDirection = sortDirection,
-                Search = search,
-            });
+            return sortedQuery.ToListAsync();
         }
 
         [HttpPost]
