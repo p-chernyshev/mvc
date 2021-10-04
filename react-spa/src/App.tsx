5bdefda896f4a6ede2831f194bc0c027915a152b
Petr Chernyshev <chernyshevpetr1995@mail.ru>
2021-10-04 23:22:53 +0500

    list component

--- react-spa/src/App.tsx
+++ react-spa/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route, Redirect } from 'react-router-dom';
 import Header from './Header';
+import List from './List';
 
 class App extends React.Component {
     public render(): React.ReactNode {
@@ -12,6 +13,7 @@ class App extends React.Component {
                     <main role="main" className="pb-3">
                         <Route exact path=""> <Redirect to="/list"/> </Route>
                         <Route path="/list">
+                            <List />
                         </Route>
                         <Route path="/cart">
                         </Route>
---
+++ react-spa/src/List.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './List.css';
+import { Disk, DISK_TYPE_DISPLAY, DISK_FORM_DISPLAY } from './types/Disk';
+
+interface ListState {
+    disks: Disk[];
+}
+
+class List extends React.Component<{ }, ListState> {
+    public async componentDidMount(): Promise<void> {
+        const response = await fetch('https://localhost:5001/Disk/IndexJson');
+        const disks = await response.json() as Disk[];
+        this.setState({ disks });
+    }
+
+    public render(): React.ReactNode {
+        return (
+            <div className="disk-table">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Название</th>
+                            <th>Размер</th>
+                            <th>Скорость записи</th>
+                            <th>Скорость чтения</th>
+                            <th>Цена</th>
+                            <th>Тип</th>
+                            <th>Формат</th>
+                            <th/>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state?.disks.map(disk => (
+                            <tr key={disk.id}>
+                                <td className="disk-table__cell-name">{disk.name}</td>
+                                <td>{disk.sizeGb} ГБ</td>
+                                <td>{disk.writeSpeedMbps} МБ/с</td>
+                                <td>{disk.readSpeedMbps} МБ/с</td>
+                                <td>{disk.price} ₽</td>
+                                <td>{DISK_TYPE_DISPLAY[disk.type]}</td>
+                                <td>{DISK_FORM_DISPLAY[disk.form]}</td>
+                                <td>
+                                    <a
+                                        className="disk-table__to-cart"
+                                    >В корзину</a>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default List;
